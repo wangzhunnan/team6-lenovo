04438cb9f7d5345353a83eb01eaf1d5a5530191c
fuhao <1137374324@qq.com>
2019-10-08 10:13:21 +0800

    问题库页面

--- src/components/Students/myQuestion.vue
+++ src/components/Students/myQuestion.vue
@@ -1,28 +1,146 @@
 <template>
-    <div class="questionsBg">
-        我的提问
+  <div class="content">
+
+    <div class="search_bg">
+      <div class="search">
+        <el-input v-model="input" placeholder="Java 最核心的基础是什么" style="width: 450px;"></el-input>
+        <button class="sousuo" type="button">搜答案</button>
+        <button type="button">我的提问</button>
+      </div>
     </div>
+
+    <div class="ask_bg">
+      <div class="my_ask">我要提问</div>
+      <div class="my_problem">
+        <p>
+          <span>*</span>问题
+        </p>
+        <el-input v-model="question" placeholder="请在这里输入你的问题 (120个字符内)" style="width: 390px;"></el-input>
+        <p>
+          <span>*</span>问题说明
+        </p>
+        <el-input type="textarea" :rows="8" placeholder="请对你的问题进行详细描述" v-model="subject"></el-input>
+        <p>问题标签</p>
+        <el-input v-model="typeName" placeholder="java,html,sql" style="width: 390px;"></el-input>
+        <span>* 用逗号分开，不超过五个，且每个标签长度不超过九个字符</span>
+        <p>当前标签</p>
+        <button type="button" @click="ask">提交问题</button>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
-export default {
-    name: 'myQuestion',
-    data(){
-        return{
-
+    export default {
+        name: "myAnswer",
+        data: function() {
+            return {
+                input: "",
+                typeName: "",
+                subject: "",
+                question: ""
+            };
+        },
+        methods: {
+            //当前组件用到的函数
+            ask: function() {
+                // console.log(this.question);
+                this.$http
+                    .post("/business/studentQuestion/submitQuestion", {
+                        typeName: this.typeName,
+                        subject: this.subject,
+                        question: this.question
+                    })
+                    .then(function(res) {
+                        console.log(res);
+                    });
+            }
+        },
+        created() {
+            //组件加载完成后的生命回调
         }
-    },
-    methods: {
-
-    },
-    created(){//页面一开始显示的数据
-
-    }
-}
+    };
 </script>
-<style>
-.questionsBg{
-    height: 100px;
-    width: 100%;
-    background: #63A1F5;
-}
+
+<style scoped>
+  /* 主体部分content */
+  /* .content{
+      width: 100%;
+      height: 800px;
+  } */
+  .content .search_bg {
+    /* width: 100%; */
+    height: 80px;
+    background: #62a1f4;
+    border: 1px solid #62a1f4;
+  }
+  .search {
+    margin-top: 20px;
+    margin-left: 440px;
+  }
+  .search button {
+    width: 120px;
+    height: 40px;
+    color: #fff;
+    border: none;
+  }
+  .search .sousuo {
+    margin-left: -5px;
+    height: 40px;
+    width: 120px;
+  }
+  .search button {
+    background: rgba(255, 255, 255, 0.26);
+    color: #fff;
+    border-color: rgba(255, 255, 255, 0.6);
+  }
+  .content .ask_bg {
+    width: 55%;
+    height: 558px;
+    margin-left: 400px;
+    /* background: #ccc; */
+    /* border: 1px solid #000; */
+  }
+  .content .ask_bg .my_ask {
+    width: 80px;
+    height: 25px;
+    /* background: #f00; */
+    line-height: 25px;
+    text-align: center;
+    border-left: 3px solid #62a1f4;
+    margin: 20px 0;
+  }
+  .content .ask_bg .my_problem {
+    /* width: 780px; */
+    /* background: #0f0; */
+    margin-left: 10px;
+    font-size: 13px;
+    color: #999;
+  }
+  .content .ask_bg .my_problem p {
+    margin: 10px 0;
+    font-size: 14px;
+    color: #606060;
+  }
+  .content .ask_bg .my_problem input {
+    width: 750px;
+    height: 30px;
+    border: 1px solid #ccc;
+    border-radius: 3px;
+  }
+  .content .ask_bg .my_problem .tag {
+    width: 400px;
+  }
+  .content .ask_bg .my_problem span {
+    color: #ff6a01;
+    font-size: 12px;
+    padding-left: 10px;
+    width: 160px;
+  }
+  .content .ask_bg .my_problem button {
+    width: 105px;
+    height: 35px;
+    background: #4abfe0;
+    color: #fff;
+    border: none;
+  }
 </style>
--- src/components/Teachers/myAnswer.vue
+++ src/components/Teachers/myAnswer.vue
@@ -1,21 +1,150 @@
 <template>
-    <div>
-        在线解答··
-    </div>
+  <div id="OnlineAnswer">
+    <el-row>
+      <el-col :span="24" class="header">
+        <div class="h-text">
+          <p class="h-t-p1">在线解答</p>
+          <p class="h-t-p2">记录问题数据 知识点问答 在线问题答疑</p>
+        </div>
+        <div class="h-img">
+          <img src="../../images/back03.png" alt />
+        </div>
+      </el-col>
+
+      <el-col :span="24">
+        <div class="mbox">
+          <p class="mb-p">问题库</p>
+
+          <template>
+            <el-tabs v-model="activeName" type="card" @tab-click="handleClick">
+              <el-tab-pane label="已回复提问" name="first">
+                <template>
+                  <el-table :data="tableData" style="width: 100%">
+                    <el-table-column prop="date" label="日期" width="180"></el-table-column>
+                    <el-table-column prop="name" label="姓名" width="180"></el-table-column>
+                    <el-table-column prop="address" label="地址"></el-table-column>
+                    <el-table-column prop="caozuo" label="详情"></el-table-column>
+                  </el-table>
+                </template>
+              </el-tab-pane>
+              <el-tab-pane label="未回复提问" name="second">
+                <template>
+                  <el-table :data="tableData" style="width: 100%">
+                    <el-table-column prop="date" label="日期" width="180"></el-table-column>
+                    <el-table-column prop="name" label="姓名" width="180"></el-table-column>
+                    <el-table-column prop="address" label="地址"></el-table-column>
+                    <el-table-column prop="caozuo" label="详情"></el-table-column>
+                  </el-table>
+                </template>
+              </el-tab-pane>
+            </el-tabs>
+          </template>
+
+          <el-divider></el-divider>
+
+          <p class="mb-p">意见反馈</p>
+
+          <template>
+            <el-table :data="tableData" style="width: 100%">
+              <el-table-column prop="date" label="意见" width="180"></el-table-column>
+              <el-table-column prop="name" label="意见类型" width="180"></el-table-column>
+              <el-table-column prop="address" label="发起时间"></el-table-column>
+              <el-table-column prop="caozuo" label="操作"></el-table-column>
+            </el-table>
+          </template>
+        </div>
+      </el-col>
+    </el-row>
+  </div>
 </template>
+
 <script>
-export default {
-    name: 'myAnswer',
-    data(){
-        return{
-
+    export default {
+        name: "OnlineAnswer",
+        data: function() {
+            return {
+                activeName: "first",
+                tableData: [
+                    {
+                        date: "2016-05-02",
+                        name: "王小虎",
+                        address: "上海市普陀区金沙江路 1518 弄",
+                        caozuo: "详情"
+                    }
+                ],
+                page: 1,
+                pageSize: 3,
+                arr: [],
+                arr1: [],
+                arr2: []
+            };
+        },
+        methods: {
+            handleClick(tab, event) {
+                // console.log(tab, event);
+            }
+        },
+        created() {
+            // 组件加载完成之后的生命回调函数,如果页面一加载就需要显示数据,数据就在此获取
+            var app = this;
+            app.$http
+                .post("/business/studentQuestion/page", {
+                    page: this.page,
+                    pageSize: this.pageSize
+                })
+                .then(function(res) {
+                    // console.log(res);
+                    app.arr = res.data.data;
+                    console.log(app.arr);
+                    for(var i=0;i<app.arr.length;i++){
+                        if(app.arr[i].isSolution === "N"){
+                            console.log('aa');
+                            app.arr1 = app.arr[i];
+                        }else{
+                            app.arr2 = app.arr[i];
+                        }
+                    }
+                    console.log("我是arr1："+app.arr1);
+                    console.log("我是arr2："+app.arr2);
+                });
         }
-    },
-    methods: {
-
-    },
-    created(){//页面一开始显示的数据
-
-    }
-}
+    };
 </script>
+
+<style>
+  /* 标题部分 */
+  .header {
+    min-height: 144px;
+    background: linear-gradient(60deg, #6cc4ce, #65f1ce);
+  }
+  .h-text {
+    float: left;
+    margin-left: 180px;
+    color: #ffffff;
+  }
+  .h-t-p1 {
+    font-size: 26px;
+    font-weight: 400;
+  }
+  .h-t-p2 {
+    font-weight: 300;
+    font-size: 16px;
+  }
+  .h-img {
+    float: right;
+    margin-right: 77px;
+  }
+
+  /* 主要内容 */
+  .mbox {
+    margin-right: 100px;
+    margin-left: 100px;
+  }
+  .mb-p {
+    height: 18px;
+    line-height: 18px;
+    font-size: 16px;
+    border-left: 4px solid rgb(74, 192, 224);
+    padding-left: 10px;
+  }
+</style>
